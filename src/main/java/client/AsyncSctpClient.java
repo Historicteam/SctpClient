66bdd579e490bbf27f4828dd760631b66fec9352
Natashkin Alex <natashkinsasha@gmail.com>
2016-03-27 20:54:33 +0300

    Добавил в асинхронный клиент метод который возврощает ответ в обертке

--- src/main/java/client/AsyncSctpClient.java
+++ src/main/java/client/AsyncSctpClient.java
@@ -1,10 +1,16 @@
 package client;
 
+import model.scresponce.SctpResponse;
 import model.stcprequest.SctpRequest;
 
 import java.io.IOException;
+import java.util.concurrent.Future;
+
 
 interface AsyncSctpClient {
     void execute(SctpRequest stcpRequest, CallBack callBack) throws IOException;
+
+    Future<SctpResponse> execute(SctpRequest stcpRequest) throws IOException;
+
     void close() throws IOException;
 }
--- src/main/java/client/AsyncSctpClientImpl.java
+++ src/main/java/client/AsyncSctpClientImpl.java
@@ -1,9 +1,15 @@
 package client;
 
+import model.scresponce.SctpResponse;
+import model.scresponce.builder.SctpResponceBytesBuilder;
 import model.stcprequest.SctpRequest;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.scheduling.annotation.AsyncResult;
 import transport.SctpRequestSender;
+import transport.SctpResponseReader;
 
 import java.io.IOException;
+import java.util.concurrent.Future;
 
 public class AsyncSctpClientImpl extends SctpClientUtil implements AsyncSctpClient {
     public AsyncSctpClientImpl(String host, int port) throws IOException {
@@ -18,10 +24,20 @@ public class AsyncSctpClientImpl extends SctpClientUtil implements AsyncSctpClie
     public void execute(SctpRequest stcpRequest, CallBack callBack) throws IOException {
         initSocket();
         initReaderThreat();
-        addToCallBackMap (stcpRequest.getId(), callBack);
-        addToRequestBackMap (stcpRequest.getId(), stcpRequest);
+        addToCallBackMap(stcpRequest.getId(), callBack);
+        addToRequestBackMap(stcpRequest.getId(), stcpRequest);
         SctpRequestSender sender = new SctpRequestSender(getOutputStream());
         sender.sendRequest(stcpRequest);
+    }
+
+    @Override
+    @Async
+    public Future<SctpResponse> execute(SctpRequest stcpRequest) throws IOException {
+        initSocket();
+        SctpRequestSender sender = new SctpRequestSender(getOutputStream());
+        SctpResponseReader reader = new SctpResponseReader(getInputStream());
+        sender.sendRequest(stcpRequest);
+        return new AsyncResult<SctpResponse>(SctpResponceBytesBuilder.build(reader.read()));
     }
 
     @Override
